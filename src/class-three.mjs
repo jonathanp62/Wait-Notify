b9742eb4e40841470ae8f9b4e0efbdedf5e1ea97
Jonathan Parker <jonathanp62@gmail.com>
2023-11-30 10:29:53 -0500

    Removed personally identifying information prior to publication

--- src/class-three.mjs
+++ src/class-three.mjs
@@ -2,9 +2,11 @@
  * (#)class-three.mjs   1.0.0   11/19/2023
  *
  * Copyright (c) Jonathan M. Parker
- * 324 Lantana Drive
- * Owings Mills, MD  21117 U.S.A
  * All Rights Reserved.
+ * 
+ * @author    Jonathan Parker
+ * @version   1.0.0
+ * @since     1.0.0
  */
 
 /**
